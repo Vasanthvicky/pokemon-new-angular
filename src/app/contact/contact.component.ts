7d2d2d27bedf8a2d70726de03b64da9e1d5daf26
vignesh vasanth <vigneshvasanth.chandrasekar@trustrace.com>
2023-06-13 23:35:43 +0530

    resume is created

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faLinkedin, faMailchimp, faPhoenixSquadron } from '@fortawesome/free-brands-svg-icons';
+import * as fabIcons  from "@fortawesome/free-solid-svg-icons";
+import { faBirthdayCake, faCalendarDays, faFlag, faHeadphonesSimple, faMailBulk, faMailReply, faVoicemail, faWindowClose } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-
+  closedIcon = faWindowClose;
+  myLinkedInIcon = faLinkedin;
+  myMailIcon = faMailBulk;
+  myPhoneIcon = faHeadphonesSimple;
+  myFlag = faFlag;
+  myDateOfBirth = faCalendarDays;
+  printIcon(daa){
+    // console.log(fabIcons[daa]);
+    return fabIcons[daa]
+    
+  }
 }
--- src/app/service/img-directory.service.ts
+++ src/app/service/img-directory.service.ts
@@ -10,230 +10,233 @@ export class ImgDirectoryService {
   myImg = [
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/Profile-img%2F0Y9A8158.JPG?alt=media&token=e3009fe2-db38-4862-9e1c-016453d09e39",
-      name: 'profile pic'
+      name: "profile pic",
     },
     // {
     //   src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/Profile-img%2FProfile-Male-PNG.png?alt=media&token=87d3b75e-71bc-4830-a919-3d8044ab03eb",
     //   name: 'profile pic2'
     // }
   ];
-  skillImg = [ 
+  skillImg = [
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2FHTML5.png?alt=media&token=958ec404-7c26-4ac4-b636-68c407b709ce",
-      name: 'HTML'
+      name: "HTML",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2FCSS3.png?alt=media&token=c8f59690-47b1-4ee2-9a73-53b73f6c44eb",
-      name: 'CSS'
+      name: "CSS",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2Fjavascript-logo.png?alt=media&token=85453924-09a3-4f1c-af19-c3c8b6b3b06f",
-      name: 'JS'
-    },    
+      name: "JS",
+    },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2Fless.png?alt=media&token=f4b8d105-23b7-4984-bc9a-9c2363cde57e",
-      name: 'Less'
-    },   
+      name: "Less",
+    },
 
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2Fsass.png?alt=media&token=d0f8a133-f492-4cbe-a316-eca625ca4312",
-      name: 'Sass'
+      name: "Sass",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2FTailwind_CSS_Logo.png?alt=media&token=96f2ea25-1164-46e7-817c-781800e42205",
-      name: 'Tailwind CSS'
+      name: "Tailwind CSS",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2Ftype-script.png?alt=media&token=94485e6d-d498-4e08-9dfb-e546a52a8d8f",
-      name: 'Type Script'
+      name: "Type Script",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2Fangular.png?alt=media&token=d5d4c9c4-d29f-4d45-b2c2-7df6c0cd33de",
-      name: 'Angular JS'
+      name: "Angular JS",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2Fgithub.png?alt=media&token=8d88d67f-aece-4649-99f4-3883a92a5edb",
-      name: 'Git Hub'
+      name: "Git Hub",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2FBitbucket.png?alt=media&token=5758a132-4972-48e9-96c3-fd57ca823097",
-      name: 'Bitbucket'
+      name: "Bitbucket",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2Fatlassian_jira_logo_icon.png?alt=media&token=dc1cc3df-db7a-49c1-89ca-861b6d8e5170",
-      name: 'Jira Service Desk'
+      name: "Jira Service Desk",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2FVisual_Studio.png?alt=media&token=673f029d-c858-47f0-9449-372fdd3e41ac",
-      name: 'Visual Studio Code'
+      name: "Visual Studio Code",
     },
     {
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/skills-img%2Fatlassian-logo.png?alt=media&token=168ad22c-d435-4509-a437-c244c1ff4252",
-      name: ''
+      name: "",
     },
   ];
 
   autoGarageInfo = [
     {
-      title: 'AUTO Garage',
+      title: "AUTO Garage",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2Fauto-garage%2FAUTO-Garage-1.png?alt=media&token=0519b2e2-345b-4658-b35c-7fa6e9e70cce",
-      description: '',
-    },
+      description:
+        "AUTO Garage is a feature rich and a customised software that digitizes the entire process of servicing the heavy vehicles at the client’s service centre. In a nutshell the application involves 6 major stages to document the process from the entry of vehicle to the exit, which is easily accessible through web and as a mobile application. Business operation and customer retention has made a drastic swift with the usage of Auto Garage within the firm. It is a complete business only operation app to eliminate the manual process with a digital management system. The client is an original equipment supplier and has a most reliable service centre for heavy duty vehicles pan India with 62+ service outlets in major cities across the country. For a developed premium service provider there was a need to simplify the end to end vehicle service procedure. Our team developed an application with a tracking system to regulate the sales and services.",
+      
+        challengesFaced: "Manual material gate pass, tedious paper works at multiple level, absence of the approval officers created a distraction in the workflow. Delay in approvals and tracking the labour working hours at the facility was time consuming and cumbersome. Things got complicated when there was raise in material request which was not in the stock. Without a proper communication customer retention was also at stake. All together it lead to the revenue leakage. The challenging factor was to introduce an automated standard operating procedure without affecting the existing working environment. Simplifying and inculcating an intelligent application in the firm was also stimulating."
+      },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2Fauto-garage%2FAUTO-Garage-2.png?alt=media&token=f11508b2-6e73-4b52-a40c-1c7d8e725a73",
-      description: '',
-    }
+      description: "",
+    },
   ];
 
   vbmsInfo = [
     {
-      title:'Vehicle Booking Management System',
+      title: "Vehicle Booking Management System",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FVehicle-Booking-Management-System%2FVIMS-1.png?alt=media&token=59d77832-0f87-47ce-af5b-1fa8df7c0a53",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FVehicle-Booking-Management-System%2FVIMS-2.png?alt=media&token=fd1e96a5-66ef-450b-b2fe-36c283be3164",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FVehicle-Booking-Management-System%2FVIMS-3.png?alt=media&token=3354a8e4-687d-4a47-acee-3607374a9560",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FVehicle-Booking-Management-System%2FVIMS-4.png?alt=media&token=d07837a2-3f97-4820-a677-ea0a3b8467a8",
-      description: '',
+      description: "",
     },
   ];
 
   roadAssistanceInfo = [
     {
-      title:'Roadside Assistance',
+      title: "Roadside Assistance",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FRoadside-Assistance%2FRSA-1.png?alt=media&token=cc926179-8eb3-43fe-878a-df9b3ab447ed",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FRoadside-Assistance%2FRSA-2.png?alt=media&token=f3987b17-9bdb-4d1b-a022-20b2aefdaa9b",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FRoadside-Assistance%2FRSA-3.png?alt=media&token=d9372d86-9a80-4365-9b54-501a9995b5e7",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FRoadside-Assistance%2FRSA-4.png?alt=media&token=8bde94a4-7fb6-481f-8518-3efa0c260f01",
-      description: '',
+      description: "",
     },
   ];
 
   pmsInfo = [
     {
-      title:'PMS',
+      title: "PMS",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FPMS%2FPMS-1.png?alt=media&token=9379f79f-d680-4309-b555-a8b3be5f7703",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FPMS%2FPMS-2.png?alt=media&token=6d83c6fd-68e2-49e2-81c7-22eb1d947fad",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FPMS%2FPMS-3.png?alt=media&token=c2c5c91f-26ba-4832-8bb2-b3bf7de330f5",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FPMS%2FPMS-4.png?alt=media&token=f709228f-6bd1-4353-a77b-12aa6ec36e84",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FPMS%2FPMS-5.png?alt=media&token=dc9090d8-2938-4c31-ae71-cee09313b926",
-      description: '',
+      description: "",
     },
   ];
   fmsInfo = [
     {
-      title:'Finance Management System',
+      title: "Finance Management System",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FFinance-Management-System%2FFMS-1.png?alt=media&token=d74e5290-cc81-40e2-9545-25b3b458b6cb",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FFinance-Management-System%2FFMS-2.png?alt=media&token=d0996e5e-f016-4d1c-9292-b7baf96c5ba3",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FFinance-Management-System%2FFMS-3.png?alt=media&token=9d9eac30-a592-4a16-8367-4ac6dea2b3a5",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FFinance-Management-System%2FFMS-4.png?alt=media&token=8abefe23-4111-456b-a405-d048c9b2cd59",
-      description: '',
-    },    
+      description: "",
+    },
   ];
 
   easyPayInfo = [
     {
-      title:'Easy Pay',
+      title: "Easy Pay",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FEasy-Pay%2FEasyPay-1.png?alt=media&token=dd4edf06-3f1c-40c2-9e1e-d0220ef82dac",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FEasy-Pay%2FEasyPay-2.png?alt=media&token=664f14ae-2b00-44df-b664-e9f6a5a9d344",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FEasy-Pay%2FEasyPay-3.png?alt=media&token=bfb5bee6-ea50-4aea-ac3a-859e7a8a3ea6",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FEasy-Pay%2FEasyPay-4.png?alt=media&token=84a51d82-929b-4811-81b1-caa34e87a0b2",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FEasy-Pay%2FEasyPay-5.png?alt=media&token=d4a352f8-f5ce-4b0f-b2fd-609b20bfc4d4",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FEasy-Pay%2FEasyPay-6.png?alt=media&token=dada2e70-44d6-4c5c-8d3e-f63741fd5e3d",
-      description: '',
+      description: "",
     },
   ];
-  demsInfo =[
+  demsInfo = [
     {
-      title:'Digital Expenses Management System',
+      title: "Digital Expenses Management System",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FDigital-Expenses-Management-System%2FDEMS-1.png?alt=media&token=73b93ba8-7ecc-4dc5-88cc-a443829de975",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FDigital-Expenses-Management-System%2FDEMS-2.png?alt=media&token=72c4b2fc-a290-4cd4-8740-609219a3974b",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FDigital-Expenses-Management-System%2FDEMS-3.png?alt=media&token=5145c0c4-68a6-42f4-89ea-bc3af0ac8b88",
-      description: '',
+      description: "",
     },
     {
-      title:'',
+      title: "",
       src: "https://firebasestorage.googleapis.com/v0/b/vigneshvasanthportfolio.appspot.com/o/project-img%2FDigital-Expenses-Management-System%2FDEMS-4.png?alt=media&token=7168c728-137d-4bb9-b2a2-93b76d201616",
-      description: '',
-    }
+      description: "",
+    },
   ];
 }
